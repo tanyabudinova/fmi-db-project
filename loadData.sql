c139b1c238485d400bf080aa456fdb7a07dc5823
tanyabudinova <tanya.budinova@gmail.com>
2020-06-13 22:29:16 +0300

    Added data for one series

---
+++ loadData.sql
@@ -0,0 +1,29 @@
+INSERT INTO SERIES (NAME, DESCRIPTION, TRAILERURL, POSTERURL) VALUES ('Gravity Falls',
+                                                                      'Twin siblings Dipper and Mabel Pines spend the summer at their great-uncle''s tourist trap in the enigmatic Gravity Falls, Oregon.',
+                                                                      'https://imdb-video.media-imdb.com/vi3628907545/1434659607842-pgv4ql-1566320783657.mp4?Expires=1592160583&Signature=sv2T8k5AaVt7GxtRAiGMH0BwVoWOwn6p3wXNR6PlY8Grt6hHwzcOUpsHtkQSaoXnOmIhgby3MAKJ7UsAkKCwqMnXUd7MuXO56B4ed7jmtU0weEigxjX3wc~MYVvlVV2b127KPs6Dw30a3FkNWIhNm0uUopeIoIIWrhU2FgQ4B6Oa7BqHaxKHpsSyYcwTmYnyz9L7DEVwRdI0aTZ4ZSWF0vcA-COTwVYMIEpWBmSUd2iLFkHCkoCCAUFH4iJDW-FOU8khwAyd3WXkZ3gxwzVlNP3qf1KNxM7uQzDdMiebT6dmRnJvRxY4T7y7J10QjNhp1ejnW9CC1qdjW6HwaLBqIA__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA',
+                                                                      'https://m.media-amazon.com/images/M/MV5BMTEzNDc3MDQ2NzNeQTJeQWpwZ15BbWU4MDYzMzUwMDIx._V1_SY1000_CR0,0,641,1000_AL_.jpg');
+
+INSERT INTO GENRE (NAME) VALUES ('Animation');
+
+INSERT INTO GENRE (NAME) VALUES ('Action');
+
+INSERT INTO GENRE (NAME) VALUES ('Adventure');
+
+INSERT INTO EPISODE (SERIESID, NAME, PRODUCERNAME, LENGTH, NUMBER, SEASONNUMBER, RATING, DESCRIPTION) VALUES (1, 'Tourist Trapped', 'Alex Hirsch', 1320, 1, 1, 0.0,
+                                                                                                              'After finding a strange book in the forest, Dipper begins to learn about the dark side of Gravity Falls, and suspects that Mabel''s new boyfriend is a zombie.');
+
+INSERT INTO ACTOR (NAME, BIRTHDATE, IMAGEURL) VALUES ('Kristen Schaal', '1978-01-24', 'https://m.media-amazon.com/images/M/MV5BMTIxNzAzNzUwM15BMl5BanBnXkFyZXN1bWU@._V1_.jpg');
+
+INSERT INTO ACTOR (NAME, BIRTHDATE, IMAGEURL) VALUES ('Jason Ritter', '1980-02-17', 'https://m.media-amazon.com/images/M/MV5BMTMzODA1NzMyOF5BMl5BanBnXkFtZTcwMTI5MDEyNg@@._V1_.jpg');
+
+INSERT INTO ACTOR (NAME, BIRTHDATE, IMAGEURL) VALUES ('Alex Hirsch', '1985-06-18', 'https://m.media-amazon.com/images/M/MV5BNTgyNjhlMmQtNGZjMi00MjE1LTg5MDEtOTRkOTU0NzY1MWEyXkEyXkFqcGdeQXVyOTYwMjczODY@._V1_.jpg');
+
+INSERT INTO EPISODE (SERIESID, NAME, PRODUCERNAME, LENGTH, NUMBER, SEASONNUMBER, RATING, DESCRIPTION) VALUES (1, 'Scary-oke ', 'Alex Hirsch', 1380, 1, 2, 0.0,
+                                                                                                              'The Pines Family resolves to get back to normal by throwing a "Mystery Shack is Back" party ... but nothing is normal in Gravity Falls!');
+
+INSERT INTO REVIEW (FROM, RATING, TEXT, SERIESID, EPISODENUMBER, EPISODESEASON) VALUES ('Edvis-1997', 7, 'I''m not a fan of hearing how characters are singing in cartoons...', 1, 1, 2);
+
+INSERT INTO REVIEW (FROM, RATING, TEXT, SERIESID, EPISODENUMBER, EPISODESEASON) VALUES ('ivyleague929', 8, 'I don''t think you understand the gravity of the situation. I was really hooked after the first episode. This show is really worth grappling.', 1, 1, 1);
+
+INSERT INTO REVIEW (FROM, RATING, TEXT, SERIESID, EPISODENUMBER, EPISODESEASON) VALUES ('Edvis-1997', 9, 'I''m very disappointing in myself because I saw this TV series only now. First episode was brilliant, funny,interesting plenty of action. One of the best TV series ever.', 1, 1, 1);
+
